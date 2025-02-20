0bfd7078c62a685e2449c5813b3162c5fdbbe31a
addisonwurtz <awurtz@salesforce.com>
2025-02-20 13:03:08 -0800

    Write proxy port value to text file

--- connect_db.py
+++ connect_db.py
@@ -1,32 +1,47 @@
 import os
 import psycopg2
-from heroku_tools import set_config_vars
 
 
 DATABASE_URL = os.environ['DATABASE_URL']
-HUB_APP_NAME = os.environ['APP_NAME']
 
-db_connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# Create a cursor object
-cursor = db_connection.cursor()
+if __name__ == "__main__":
 
-# Query the 'app_config' table for the entry with the key 'PROXY_PORT'
-query = "SELECT value FROM app_config WHERE key = %s;"
-cursor.execute(query, ('PROXY_PORT',))
+    db_connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-# Fetch the result
-result = cursor.fetchone()
+    # Create a cursor object
+    cursor = db_connection.cursor()
 
-# Check if a value was found
-if result:
-    proxy_port = result[0]
-    print(f"PROXY_PORT: {proxy_port}")
-    set_config_vars(app_name=HUB_APP_NAME, config_vars={"PROXY_PORT": proxy_port})
+    # Query the 'app_config' table for the entry with the key 'PROXY_PORT'
+    query = "SELECT value FROM app_config WHERE key = %s;"
+    cursor.execute(query, ('PROXY_PORT',))
 
-else:
-    print("PROXY_PORT not found in app_config table.")
+    # Fetch the result
+    result = cursor.fetchone()
 
-# Close the cursor and connection
-cursor.close()
-db_connection.close()
+    # Check if a value was found
+    if result:
+        proxy_port = result[0]
+        print(f"PROXY_PORT: {proxy_port}")
+
+        # Write value to text file
+        with open("proxy_port.txt", "w") as file:
+            file.write(proxy_port)
+        print("Proxy port value successfully written to file.")
+
+        # Close the cursor and connection
+        cursor.close()
+        db_connection.close()
+
+        # Exit and return proxy port 
+        exit(1)
+
+    else:
+        print("PROXY_PORT not found in app_config table.")
+        
+        # Close the cursor and connection
+        cursor.close()
+        db_connection.close()
+
+        # Exit with value indicating failure
+        exit(0)
